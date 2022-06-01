6075ebf80abbbba69a57afda3f5821f2528fb163
gharnie <62819684+gharnie@users.noreply.github.com>
2022-06-01 22:24:24 +0100

    Create 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        print("{}".format(chr(ord(i) - 32) if i >= "a"
+                          and i <= "z" else i), end="")
+    print()
+    
