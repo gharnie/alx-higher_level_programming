1d9c4b81a716cafb841a09739d0e8be8fe2291d6
gharnie <bamigbopaganiyu1@gmail.com>
2022-06-04 06:58:43 -0700

    my commit

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    return None
+for i in range(len(my_list)):
+    if my_list[i] == my_list[idx]:
+        return my_list[idx]:
+            return my-list[idx]
---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, element):
+    if (idx < 0 or idx > len(my_list) - 1):
+        retuurn my_list
+        for i in range(len(my_list)):
+            if my_list[i] == my_list[idx]:
+                my_list[idx] = element
+                reyturn my_list
